ab525847bdc16a7c1255310afa3559aaede127aa
ksoftdev <kuntursoftdev@gmail.com>
2020-05-02 15:31:46 -0500

    Fixed LFTerm

--- LFTerm.py
+++ LFTerm.py
@@ -3,7 +3,7 @@ import os
 def mostrarG(G):
     for k, v in G.items():
         print(k, ': ', v)
-        
+
 def mostrarTerminables(TERM):
     print( '{' + ''.join(TERM) + '}')
 
@@ -12,42 +12,38 @@ def calcularTerminables(G, V):
     TERM = []
     C = TERM
 
-    i = 1
-    while ( True ):
-        if ( i == 1 ):
+    for k, v in NTERM.copy().items():
+        for val in v:
+            if not any( item in V for item in list(val) ) or val == 'lambda':
+                TERM.append(k)
+                del NTERM[k]
+
+    if ( TERM ):
+        while ( C ):
+            print('\nTERM')
+            print(TERM)
+            print('\nNTERM')
+            mostrarG(NTERM)
+
+            C = []
+
             for k, v in NTERM.copy().items():
-            	for val in v:
-                	if not any( item in V for item in list(val) ) or val == 'lambda':
-                	    TERM.append(k)
-                	    del NTERM[k]
-        else:
-            while ( C ):
-                print('\nTERM')
-                print(TERM)
-                print('\nNTERM')
-                mostrarG(NTERM)
-                
-                C = []
-                
-                for k, v in NTERM.copy().items():
-                    for prod in v:
-                        isterm = True
-                        for item in prod:
-                            if ( item in V ):
-                                isterm = isterm and ( item in TERM )
-                        if ( isterm ):
-                            C.append(k)
-                            del NTERM[k]
-                            break
-                TERM += C
-            break
-        i += 1
+                for prod in v:
+                    isterm = True
+                    for item in prod:
+                        if ( item in V ):
+                            isterm = isterm and ( item in TERM )
+                    if ( isterm ):
+                        C.append(k)
+                        del NTERM[k]
+                        break
+            TERM += C
     return NTERM
 
 if __name__ == '__main__':
     G = {}
     V = []
-    
+
     n_prod = int(input())
     for i in range(n_prod):
         line =  input().split('->')
@@ -57,7 +53,7 @@ if __name__ == '__main__':
             v[i] = v[i].rstrip().lstrip()
         V.append(k)
         G.update({k : v})
-    
+
     mostrarG(G)
     TERM = calcularTerminables(G, V)
     mostrarTerminables(TERM)
